33ee3fc84c8ffb875470846137f171991859d797
leo-dosu <wash4wear@gmail.com>
2023-11-09 07:18:20 +0100

    Update: navbar-responsive completed call-log-ongoing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileSettingsComponent } from './profile-settings/profile-settings.component';
+import { CallLogsComponent } from './call-logs/call-logs.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent}, 
   {path:'login', component: LoginComponent},
   {path: 'profile-settings', component: ProfileSettingsComponent},
+  {path: 'call-logs', component: CallLogsComponent}
 
 ];
 
---
+++ src/app/call-logs/call-logs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-call-logs',
+  templateUrl: './call-logs.component.html',
+  styleUrls: ['./call-logs.component.scss']
+})
+export class CallLogsComponent {
+
+}
