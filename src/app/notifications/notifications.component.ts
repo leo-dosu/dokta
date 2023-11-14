37d17e3ecb7f749e76c423bf9667ddcf3a2493af
leo-dosu <wash4wear@gmail.com>
2023-11-14 04:46:42 +0100

    UPDATE: Notifications screen added

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class NotificationsComponent {
 
+  isSwitchOn: boolean = false;
+
+  toggleSwitch(): void {
+    this.isSwitchOn = !this.isSwitchOn;
+  }
+
 }
